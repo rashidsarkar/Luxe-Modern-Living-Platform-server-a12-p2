d00a72f0ae79e183ba8b93031e89706744fbcb4d
rashidsarkar <rashidrock558@gmail.com>
2023-11-28 16:27:33 +0600

    code change with new

---
+++ src/middlewares/applyMiddleware.js
@@ -0,0 +1,15 @@
+const cors = require("cors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { LOCAL_CLIENT, CLIENT } = require("../config/default");
+const applyMiddleWare = (app) => {
+  app.use(
+    cors({
+      origin: [LOCAL_CLIENT, CLIENT],
+      credentials: true,
+    })
+  );
+  app.use(express.json());
+  app.use(cookieParser());
+};
+module.exports = applyMiddleWare;
